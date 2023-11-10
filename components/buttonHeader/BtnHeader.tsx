cd129a0f094994dccc8ecede0fb0c721ba019879
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-11-11 01:55:07 +0800

    delete auth

--- components/buttonHeader/BtnHeader.tsx
+++ components/buttonHeader/BtnHeader.tsx
@@ -1,6 +1,4 @@
 import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   Popover,
   PopoverContent,
--- components/ui/alert-dialog.tsx
+++ components/ui/alert-dialog.tsx
@@ -139,3 +139,4 @@ export {
   AlertDialogAction,
   AlertDialogCancel,
 }
+
