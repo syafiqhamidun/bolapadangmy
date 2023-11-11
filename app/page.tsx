fe87ae3f253861a0c9fc3c558c24a956b6f749fa
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-11-12 04:04:50 +0800

    repair page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Header from "@/components/header"
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createServerComponentClient({cookies});
+  const {data, error} = await supabase.auth.getUser()
+  console.log("The data is" , data);
+
   return (
     <main>
       <Header/>
