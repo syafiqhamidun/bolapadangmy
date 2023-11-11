bf94989f4aaf3576dfd6dca103671483cf246ac7
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-11-12 03:58:45 +0800

    signup done

--- components/aut/SignupModal.tsx
+++ components/aut/SignupModal.tsx
@@ -19,19 +19,50 @@ import { useState } from "react";
 import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import { RegisterType, registerSchema } from "@/validations/authSchemas";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
- const SignupModal = () => {
+export default function SignupModal () {
+        const supabase = createClientComponentClient();
         const [open, setOpen] = useState<boolean>(false);
+        const [loading, setLoading] =useState<boolean>(false);
+        const router= useRouter()
         const {
             register,
             handleSubmit,
             formState: { errors },
-          } = useForm({
+          } = useForm<RegisterType>({
             resolver: yupResolver(registerSchema),
           })
-          const onSubmit = (data:RegisterType) => {
-            console.log(data)
+          
+          const onSubmit = async (payload:RegisterType) => {
+            setLoading(true)
+            const {data, error} = await supabase.auth.signUp({
+                email: payload.email,
+                password: payload.password,
+                options: {
+                    data: {
+                        name: payload.name
+                    },
+                },
+            });
+
+            setLoading(false)
+            if (error) {
+                toast.error(error.message, {theme: "colored"});
+            } else if (data.user) {
+                await supabase.auth.signInWithPassword({
+                    email: payload.email,
+                    password: payload.password,
+                });
+                setOpen(false);
+                router.refresh();
+                toast.success("Logged in successfully", {theme: "colored"})
+            }
+
         };
 
     return ( 
@@ -51,40 +82,55 @@ import { RegisterType, registerSchema } from "@/validations/authSchemas";
                             <X className="cursor-pointer" onClick={() => setOpen(false)}/>
                         </div>
                     </AlertDialogTitle>
-                    <AlertDialogDescription>
+                    <AlertDialogDescription asChild>
                         <form onSubmit={handleSubmit(onSubmit)}>
+                        <ToastContainer/>
+                        {/* Name */}
+                        
                         <div className="mt-5">
                                 <Label htmlFor="name">Name</Label>
-                                <Input placeholder="Enter your name" id="name" {...register("name")}/>
+                                <Input 
+                                    placeholder="Enter your name" 
+                                    id="name" 
+                                    {...register("name")}/>
                                 <span className="text-red-400">{errors.name?.message}</span>
                             </div>
 
+                        {/* Email */}
+
                             <div className="mt-5">
                                 <Label htmlFor="email">Email</Label>
-                                <Input placeholder="Enter your email" id="email" {...register("email")}/>
+                                <Input 
+                                placeholder="Enter your email" 
+                                id="email" 
+                                {...register("email")}/>
                                 <span className="text-red-400">{errors.email?.message}</span>
                             </div>
-                        </form>
-                    </AlertDialogDescription>
 
-                    <AlertDialogDescription>
-                        <form>
+                        {/* Password */}
+
                             <div className="mt-5">
                                 <Label htmlFor="password">Password</Label>
-                                <Input placeholder="Enter your password" id="password" {...register("password")}/>
+                                <Input placeholder="Enter your password" id="password" type="password" {...register("password")}/>
                                 <span className="text-red-400">{errors.password?.message}</span>
                             </div>
 
+                        {/* Confirm Password */}
+
                             <div className="mt-5">
                                 <Label htmlFor="cpassword">Confirm Password</Label>
-                                <Input placeholder="Confirm your password" id="cpassword" {...register("password_confirmation")}/>
+                                <Input placeholder="Confirm your password" id="cpassword" type="password" {...register("password_confirmation")}/>
                                 <span className="text-red-400">{errors.password_confirmation?.message}</span>
                             </div>
 
+                        {/* Continue Button */}
+
                             <div className="mt-5">
-                                <Button className="bg-red-400 w-full">Continue</Button>
+                                <Button className="bg-red-400 w-full" disabled={loading}>{loading ? "Processing" : "Continue"}</Button>
                             </div>
                             <h1 className="text-center my-2 text-xl font-bold"> -- OR --</h1>
+
+                        {/* Google */}
 
                            <Button variant={"outline"} className="w-full">
                                 <Images
@@ -106,6 +152,4 @@ import { RegisterType, registerSchema } from "@/validations/authSchemas";
         </AlertDialog>
 
      );
- }
-  
- export default SignupModal;
+ };
--- validations/authSchemas.ts
+++ validations/authSchemas.ts
@@ -3,8 +3,8 @@ import * as yup from "yup"
 export const registerSchema = yup
   .object({
     name: yup.string().required().min(3).max(50),
-    email: yup.string().email(),
-    password: yup.string().min(6).max(15),
+    email: yup.string().email().required(),
+    password: yup.string().min(6).max(15).required(),
     password_confirmation: yup
         .string()
         .required()
