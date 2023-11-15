97149fd7717a55654ef960e4f32f05c4c848c5b2
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-11-15 15:56:51 +0800

    add homepage

--- components/common/counter.tsx
+++ components/common/counter.tsx
@@ -1,14 +1,30 @@
 "use client";
 
 import React, {useEffect, useState} from 'react'
+import AnimatedNumbers from "react-animated-numbers";
 
-export default function counter() {
-    const [state, setState] = useState(false)
-    useEffect(() => {
-        setState(true)
-    },[]); 
+export default function counter({num}:{num:number}) {
+  const [state, setState] = useState(false)
+  useEffect(() => {
+    setState(true)
+  },[]);
 
   return (
-    <div>counter</div>
+    <div>
+      {state && <AnimatedNumbers
+        includeComma
+        animateToNumber={num}
+        fontStyle={{ fontSize: 40, fontWeight: "bold" }}
+        locale="en-US"
+        configs={[
+          { mass: 1, tension: 220, friction: 100 },
+          { mass: 1, tension: 180, friction: 130 },
+          { mass: 1, tension: 280, friction: 90 },
+          { mass: 1, tension: 180, friction: 135 },
+          { mass: 1, tension: 260, friction: 100 },
+          { mass: 1, tension: 210, friction: 180 },
+        ]}
+      ></AnimatedNumbers>}
+    </div>
   )
 }
