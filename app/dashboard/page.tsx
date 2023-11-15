cf7acbb4f0a71e6e81a5733d0d4e21971223bec7
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-11-16 04:50:25 +0800

    edit dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,56 @@
 import React from 'react'
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table"
+import NavBar from '@/components/base/NavBar'
+import { createClientComponentClient, createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+  
 
-export default function page() {
+export default async function Dashboard() {
+    const supabase = createServerComponentClient({cookies})
+    const user = await supabase.auth.getUser()
+    const {data, error} = await supabase.from("homes").select("id, image, title, state, city, contact_number, created at")
+    .eq("user_id" , user.data.user?.id)
+
   return (
-    <div>page</div>
+    <div>
+        <NavBar/>
+        <div className='container mt-10'>
+            <Table>
+                <TableCaption>Your Football Fields List</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>State</TableHead>
+                        <TableHead>City</TableHead>
+                        <TableHead>Title</TableHead>
+                        <TableHead>Image</TableHead>
+                        <TableHead>Contact Number</TableHead>
+                        <TableHead className='text-right'>Action</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                {homes && homes.map((item) => (
+                    <TableRow>
+                        <TableCell className="font-medium">INV001</TableCell>
+                        <TableCell>Paid</TableCell>
+                        <TableCell>Credit Card</TableCell>
+                        <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow>
+
+                ))}
+
+                </TableBody>
+            </Table>
+        </div>
+  
+
+    </div>
   )
 }
