c3ab9cd7d8fb7f50ac41797693a6b91a2e332784
Syafiq Hamidun <syafiqhamidun10@gmail.com>
2023-11-19 20:58:56 +0800

    refix error on dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,53 +1,40 @@
+
 import React from 'react'
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 import NavBar from '@/components/base/NavBar'
-import { createClientComponentClient, createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-  
 
-export default async function Dashboard() {
-    const supabase = createServerComponentClient({cookies})
-    const user = await supabase.auth.getUser()
-    const {data : homes} = await supabase.from("homes").select("id, image, title, state, city, contact_number, created at")
-    .eq("user_id" , user.data.user?.id)
 
+export default function dashboard() {
   return (
     <div>
-        <NavBar/>
-        <div className='container mt-10'>
-            <Table>
-                <TableCaption>Your Football Fields List</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>State</TableHead>
-                        <TableHead>City</TableHead>
-                        <TableHead>Title</TableHead>
-                        <TableHead>Image</TableHead>
-                        <TableHead>Contact Number</TableHead>
-                        <TableHead className='text-right'>Action</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                {homes.map((item) => (
-                <TableRow key={item.state}>
-                  <TableCell>{item.city}</TableCell>
-                  <TableCell>{item.title}</TableCell>
-                  <TableCell>{item.image}</TableCell>
-                  <TableCell>{item.price}</TableCell>
-                </TableRow>
-              ))}
-                </TableBody>
-            </Table>
-        </div>
-  
+      <NavBar/>
+      <Table>
+  <TableCaption>A list of your recent invoices.</TableCaption>
+  <TableHeader>
+    <TableRow>
+      <TableHead className="w-[100px]">Invoice</TableHead>
+      <TableHead>Status</TableHead>
+      <TableHead>Method</TableHead>
+      <TableHead className="text-right">Amount</TableHead>
+    </TableRow>
+  </TableHeader>
+  <TableBody>
+    <TableRow>
+      <TableCell className="font-medium">INV001</TableCell>
+      <TableCell>Paid</TableCell>
+      <TableCell>Credit Card</TableCell>
+      <TableCell className="text-right">$250.00</TableCell>
+    </TableRow>
+  </TableBody>
+</Table>
 
     </div>
   )
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,4 @@
+import Env from "@/config/env";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
@@ -14,4 +15,8 @@ export function generateRandomNumber(): number {
   const min = 2000;
   const max = 20000;
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function getBPImageUrl(path:string):string {
+  return `${Env.SUPABASE_URL}}/storage/v1/object/public/BolaPadang.MY/1700076427287_13039`
 }
